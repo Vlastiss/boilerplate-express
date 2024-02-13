ee50043f95c81ab783cf27595518035366112a2d
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-02-13 17:46:25 +0000

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello Express")
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello Express")
+// })
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+})
 
 console.log("Hello World!")
 
